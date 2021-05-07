642ea857f0536f37587b36dc66a57464fa52c0c9
Alexandr Smagin <smaginalexander@yandex.ru>
2021-05-07 11:29:14 +0300

    Updates

---
+++ precache-manifest.b1ae3428f98b1c13ab7e1aaf305b11fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bc6c2664f7062f9a41841e14fec489",
+    "url": "/smaginalexander/crud-ui.git/index.html"
+  },
+  {
+    "revision": "60863c3a89f26f6e4992",
+    "url": "/smaginalexander/crud-ui.git/static/css/main.8cd19095.chunk.css"
+  },
+  {
+    "revision": "eb3fcca4d3c60c6c4c7e",
+    "url": "/smaginalexander/crud-ui.git/static/js/2.79e5bc13.chunk.js"
+  },
+  {
+    "revision": "60863c3a89f26f6e4992",
+    "url": "/smaginalexander/crud-ui.git/static/js/main.c8907e6f.chunk.js"
+  },
+  {
+    "revision": "26213b527bdb1fa58219",
+    "url": "/smaginalexander/crud-ui.git/static/js/runtime-main.1499c2f2.js"
+  }
+]);
